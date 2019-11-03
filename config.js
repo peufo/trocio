3504b8100a08a796281449fbc3de79ee57c207ed
Xerthes <ralph.periat@gmail.com>
2019-11-03 18:42:16 +0100

    Add config SuperAdmin

--- config.js
+++ config.js
@@ -6,6 +6,8 @@ module.exports = {
 	DBPATH: dev ? `mongodb://localhost:27017/${pkg.name}-dev` : `mongodb://localhost:27017/${pkg.name}`,
 	SECRET: `~WB)WNti,RICB|>Kjg!.@Pg_m4Vw:Zhm^&~wW.75$='1H$6qYY)%Fc==PK13i8c`,
 
+	TROCIO_ADMIN: `trocio@trocio.com`,
+
 	//Config SMTP
 	SMTP_HOST: `localhost`, // address of your SMTP server
 	SMTP_PORT: 465, // port of your SMTP server
