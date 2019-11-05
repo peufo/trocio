c1c9dbf4651bf05af2f9c21cfee9b5aec385dbe5
PeufOne <jonas.voisard@gmail.com>
2019-11-05 22:22:14 +0100

    FIX

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'node_modules', '@material')))
 app.use(session({
-	secret: SECRET_STRING_COOKIE,
+	secret: process.env.SECRET_STRING_COOKIE,
 	cookie: {maxAge: 72*60*60*1000},
   store: new MongoStore({mongooseConnection: mongoose.connection}),
 	resave: false,
