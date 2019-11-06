2135e5669a08064cde5cb349736fcc4cdd316d62
PeufOne <jonas.voisard@gmail.com>
2019-11-06 20:49:14 +0100

    add env var TROCIO_DB for personalize DB name

--- config.js
+++ config.js
@@ -13,10 +13,18 @@ var dev = process.env.NODE_ENV == 'development'
 *	SMTP_USER               user of your SMTP server						=>	postmaster@trocio.ch
 *	SMTP_PASS               password of your SMTP							=>	1234XYZ
 *	BASE_MAIL_URL           basic link send in the mails					=>	https://localhost:3001/users/validmail
+*	(TROCIO_DB)				personalized DB name							=>	mongodb://localhost:27017/trocio
 *
 */
 
+let DBPATH = ''
+if (process.env.TROCIO_DB) {
+	DBPATH = `mongodb://localhost:27017/${process.env.TROCIO_DB}`
+} else {
+	DBPATH = dev ? `mongodb://localhost:27017/${pkg.name}-dev` : `mongodb://localhost:27017/${pkg.name}`
+}
+
 module.exports = {
 	PORT: dev ? 3001 : 3000,
-	DBPATH: dev ? `mongodb://localhost:27017/${pkg.name}-dev` : `mongodb://localhost:27017/${pkg.name}`,
+	DBPATH,
 }
