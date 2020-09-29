3cf9a866a9f947db841230baf71460f5415ed408
PeufOne <jonas.voisard@gmail.com>
2020-09-29 20:24:07 +0200

    use dotenv finaly

--- config.js
+++ config.js
@@ -1,9 +1,21 @@
-const defaultConfig = require('./config.default.js')
+require('dotenv').config()
+let dev = process.env.NODE_ENV == 'development'
 
-//This file is ignored by git
+//Edit your configuration on .env file
 
-const config = {
-    
+const defaultConfig = {
+	TROCIO_OCD_API_KEY: 			'', 					//API key provide by https://opencagedata.com/
+	TROCIO_ADMIN: 					'',						//your mail address
+	TROCIO_SECRET_STRING_COOKIE:	'',						//A secret string for secure cookies
+	TROCIO_SMTP_PASS:				'',						//password of your SMTP server
+	TROCIO_SMTP_USER:				'postmaster@trocio.ch',	
+	TROCIO_SMTP_HOST:				'mail.infomaniak.com',	
+	TROCIO_SMTP_PORT:				587,					
+	TROCIO_PORT:					5000,									
+	TROCIO_HOST:					dev ? 'localhost:5000' : 'https://trocio.ch',			
+	TROCIO_DB:						'mongodb://localhost:27017/trocio',	
+	TROCIO_DEV: 					dev,
+	TROCIO_OPTION_FREE_TROC:		5,
 }
 
-module.exports = {...config, ...defaultConfig}
+module.exports = {...defaultConfig, ...process.env}
