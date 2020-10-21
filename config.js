1331d92a01b7157aefef8ae056396bd659f8a8d4
PeufOne <jonas.voisard@gmail.com>
2020-10-21 20:27:21 +0200

    Add test deploy

--- config.js
+++ config.js
@@ -5,16 +5,16 @@ const dev = process.env.NODE_ENV == 'development'
 
 const defaultConfig = {
 	TROCIO_OCD_API_KEY: 			'', 					//API key provide by https://opencagedata.com/
-	TROCIO_ADMIN: 					'',						//your mail address
+	TROCIO_ADMIN: 					'',						//Root user mail address
 	TROCIO_SECRET_STRING_COOKIE:	'',						//A secret string for secure cookies
 	TROCIO_SMTP_PASS:				'',						//password of your SMTP server
 	TROCIO_SMTP_USER:				'postmaster@trocio.ch',	
 	TROCIO_SMTP_HOST:				'mail.infomaniak.com',	
 	TROCIO_SMTP_PORT:				587,					
 	TROCIO_FRONT_PORT:				5000,
-	TROCIO_FRONT_HOST:				'http://localhost:5000',
+	TROCIO_FRONT_HOST:				`http://localhost:${process.env.TROCIO_FRONT_PORT || 5001}`, //TODO: remove
 	TROCIO_API_PORT:				5001,							
-	TROCIO_API_HOST:				'http://localhost:5001',			
+	TROCIO_API_HOST:				`http://localhost:${process.env.TROCIO_API_PORT || 5001}`, //TODO: remove		
 	TROCIO_DB:						'mongodb://localhost:27017/trocio',	
 	TROCIO_BACKUP:					'../dump',
 	TROCIO_DEV: 					dev,
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,30 @@
 module.exports = {
   apps : [{
-    name: 'Trocio',
+    name: 'trocio',
     script: 'server.js',
     watch: '.'
   }, {
-    name: 'Trocio - API',
+    name: 'trocio-api',
     script: './api/server.js',
     watch: './api'
   }],
   deploy : {
+    test: {
+      user : 'vt0qn_peuf',
+      host : '83.166.136.114',
+      ref  : 'origin/master',
+      repo : 'https://github.com/peufone/trocio',
+      path : '/home/clients/1f05c1e3c7f01925bd64b68ab5108107/trocio',
+      'pre-deploy-local': 'npm install && npm run build',
+      'post-deploy' : 'pm2 reload ecosystem.config.js --env production',
+      'pre-setup': 'npm install -g pm2'
+    },
     production : {
       user : '914ed_peuf',
       host : '83.166.136.114',
       ref  : 'origin/master',
       repo : 'https://github.com/peufone/trocio',
       path : '/home/clients/b0520325983e21320206249b092ee8d1/trocio',
-      'ssh_options': ["ForwardAgent=yes","StrictHostKeyChecking=no"],
       'pre-deploy-local': 'npm install && npm run build',
       'post-deploy' : 'pm2 reload ecosystem.config.js --env production',
       'pre-setup': 'npm install -g pm2'
