135f1842ac90d13bc1a8dfa4e92713ad22f0a278
PeufOne <jonas.voisard@gmail.com>
2020-10-30 23:16:25 +0100

    try config deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       path : '/home/clients/1f05c1e3c7f01925bd64b68ab5108107/trocio',
       'pre-deploy-local': 'npm install && npm run build',
       'post-deploy' : 'pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '[ -e ".nvm/.profile" ] && . ".nvm/.profile" && install -g pm2'
+      'pre-setup': '. ".nvm/.profile" && install -g pm2'
     },
     production : {
       user : '914ed_peuf',
