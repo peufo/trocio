3067f9628fba0a6563fbe7f4cde54be9fae6d809
PeufOne <jonas.voisard@gmail.com>
2020-10-31 11:13:02 +0100

    try

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,5 @@
+const CLIENT_HOME_TEST = '/home/clients/1f05c1e3c7f01925bd64b68ab5108107'
+
 module.exports = {
   apps : [{
     name: 'trocio',
@@ -14,10 +16,9 @@ module.exports = {
       host : '83.166.136.114',
       ref  : 'origin/master',
       repo : 'https://github.com/peufone/trocio',
-      path : '/home/clients/1f05c1e3c7f01925bd64b68ab5108107/trocio',
-      'pre-deploy-local': 'npm install && npm run build',
-      'post-deploy' : '. /home/clients/1f05c1e3c7f01925bd64b68ab5108107/.nvm/.profile && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '. .profile && npm install -g pm2'
+      path : `${CLIENT_HOME_TEST}/trocio`,
+      'post-deploy' : `. ${CLIENT_HOME_TEST}/.nvm/.profile; npm i; npm run build; pm2 startOrRestart ecosystem.config.js --env production`,
+      'pre-setup': '. .profile; npm install -g pm2'
     },
     production : {
       user : '914ed_peuf',
