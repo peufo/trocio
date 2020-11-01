d768585868bf906b4690b30b4a39a7ffdb724b6e
PeufOne <jonas.voisard@gmail.com>
2020-11-01 17:34:44 +0100

    Add secure context "https"

--- .nolluprc.js
+++ .nolluprc.js
@@ -3,6 +3,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = {
 	before: app => app.use('/api', createProxyMiddleware({pathRewrite: {'^/api': '/'}, target: TROCIO_API_HOST, changeOrigin: true})),
+	https: true,
+	key: 'c:/users/jonas/.ssl/localhost.key',
+	cert: 'c:/users/jonas/.ssl/localhost.crt',
 	hot: true,
 	contentBase: 'dist',
 	publicPath: 'build',
