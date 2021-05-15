89b4030e5f61b77e1670abbb01f22973df4a93a2
PeufOne <jonas.voisard@gmail.com>
2021-05-15 14:12:18 +0200

    Login page + redirect callback

--- src/pages/profile.svelte
+++ src/pages/profile.svelte
@@ -1,46 +1,13 @@
 <!-- routify:options isFooterDisplay=true -->
 <script>
-  import { Button, Icon, Card } from 'svelte-materialify'
+  import { redirect } from '@roxi/routify'
 
-  import Login from '$lib/form/Login.svelte'
   import Profile from '$lib/form/Profile.svelte'
   import { user } from '$lib/store/user'
-
-  export let scoped
 </script>
 
-{#if $user}
-  <Profile />
+{#if !$user}
+  {$redirect('/login', { callback: '/profile' })}
 {:else}
-  <div class="container" style="height: {scoped.mainHeight}px;">
-    <div class="half w3-center">
-      <a href="/">
-        <Button>
-          <Icon class="fas fa-home" />&nbsp; Retour à l'acceuil
-        </Button>
-      </a>
-      <br /><br /><br />
-      <a href="/search">
-        <Button>
-          <Icon class="fas fa-search" />&nbsp; Chercher des Trocs
-        </Button>
-      </a>
-    </div>
-    <div class="half">
-      <Card class="pa-4">
-        <Login />
-      </Card>
-    </div>
-  </div>
+  <Profile />
 {/if}
-
-<style>
-  .container {
-    max-width: 1200px;
-    margin: auto;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    flex-wrap: wrap-reverse;
-  }
-</style>
