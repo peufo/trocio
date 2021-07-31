7a670f6b4355f66ee9a5624d3a35856e4ac20280
PeufOne <jonas.voisard@gmail.com>
2021-07-31 21:27:35 +0200

    config server prod

---
+++ server.ts
@@ -0,0 +1,26 @@
+import path from 'path'
+import express from 'express'
+import logger from 'morgan'
+
+import config from './config'
+import api from './api/app'
+
+const app = express()
+
+// Activation des logs TODO: sorti dans le file system ?
+app.use(logger('dev'))
+
+// Sert l'API
+app.use('/api', api)
+
+// Sert le build
+app.use(express.static('./dist'))
+
+// Par défault, sert la SPA
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('./dist/index.html'))
+})
+
+app.listen(config.TROCIO_PORT, () => {
+  console.log(`Server listen on port ${config.TROCIO_PORT}`)
+})
