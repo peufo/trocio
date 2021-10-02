6ed07627781a2e288b6e05ca300328df3254907b
PeufOne <jonas.voisard@gmail.com>
2021-10-02 15:34:50 +0200

    Search by user wok fine

--- types/magic.d.ts
+++ types/magic.d.ts
@@ -1,5 +1,7 @@
 import type { IconDefinition } from '@fortawesome/free-solid-svg-icons'
 
+import { DynamicQuery } from 'params'
+
 /** Option of menu */
 export interface EnumOption {
   /** Valeur envoyé par la requête */
@@ -32,11 +34,12 @@ export interface FieldInteface<Type = any> {
    * Formate la valeur afficher. 'string' par défaut
    * Adapte le menu de l'entête
    */
-  format: 'string' | 'enum' | 'select' | 'currency' | 'date'
+  format: 'string' | 'user' | 'enum' | 'select' | 'currency' | 'date'
   /**
    * Clé utilisé pour le query de l'url.
    * Est utilisé pour obtenir la valeur si getValue n'est pas défini
    */
+  // queryKey: Record<`${DynamicQuery}_${keyof Type}`, string | number>
   queryKey: string
   /** Clé ou fonction retournant la valeur à afficher */
   getValue?: (value: Type) => string | number | undefined
