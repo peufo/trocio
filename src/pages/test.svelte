bb96cd93dd714183e218fe0abde20f9d57275092
PeufOne <jonas.voisard@gmail.com>
2021-10-02 13:43:07 +0200

    Magic seect work fine

---
+++ src/pages/test.svelte
@@ -0,0 +1,20 @@
+<script>
+  import MagicSelect from '$lib/util/MagicSelect.svelte'
+</script>
+
+<div class="pa-5 ma-5">
+  <MagicSelect
+    flatMode
+    keepValue
+    path="/users/search"
+    searchKey="q"
+    selectKey="provider"
+    getValue2={(item) => item.mail}
+  />
+</div>
+
+<style>
+  div {
+    border: 1px solid #ccc;
+  }
+</style>
