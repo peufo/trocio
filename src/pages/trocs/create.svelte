509fcc786baa5e87f3f4f0e0f5236a865fa0e885
PeufOne <jonas.voisard@gmail.com>
2021-10-20 00:43:11 +0200

    Add alert

--- src/pages/trocs/create.svelte
+++ src/pages/trocs/create.svelte
@@ -1,5 +1,15 @@
 <script>
+  import { Alert, Icon } from 'svelte-materialify'
+  import { mdiAlert } from '@mdi/js'
   import TrocEdit from '$lib/troc/Edit.svelte'
 </script>
+
+<Alert class="orange white-text" dismissible>
+  <div slot="icon">
+    <Icon path={mdiAlert} />
+  </div>
+  La création d'un troc est une fonctionalité réservé au testeurs. Contactez-nous
+  si vous souhaitez participer à la beta.
+</Alert>
 
 <TrocEdit createMode />
