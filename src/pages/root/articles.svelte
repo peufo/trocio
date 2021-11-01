93eeee79339b3eb956637b472b859716441f0f8b
PeufOne <jonas.voisard@gmail.com>
2021-11-01 21:11:35 +0100

    Cleanup articles func

---
+++ src/pages/root/articles.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Button } from 'svelte-materialify'
+
+  import { api } from '$lib/api'
+</script>
+
+<h3>Articles</h3>
+
+<Button
+  on:click={() =>
+    api('/api/root/clean-up-articles', {
+      method: 'post',
+    })}
+>
+  Cleanup articles
+</Button>
