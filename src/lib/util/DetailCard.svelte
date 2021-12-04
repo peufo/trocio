63dbd59df14338d1e33a97ae85df4d3519de3207
PeufOne <jonas.voisard@gmail.com>
2021-12-04 22:23:14 +0100

    Remove w3 css phase 1

--- src/lib/util/DetailCard.svelte
+++ src/lib/util/DetailCard.svelte
@@ -1,85 +1,60 @@
 <script>
   /**
-   * Carte avec entête sytétique
+   * Carte avec entête syntétique
    * Peux se déployer pour révéler des détails
    */
   import { createEventDispatcher } from 'svelte'
   import { slide } from 'svelte/transition'
-  import { List, ListItem } from 'svelte-materialify'
+  import { Card, Button, Icon } from 'svelte-materialify'
+  import { mdiChevronRight } from '@mdi/js'
 
   import { renderAmount } from '$lib/utils'
 
   export let count = 0
   export let title = ''
   export let sum = 0
-  export let free = false
-  export let show = false
-  export let items = []
-  export let nonInteractive = false
+
+  export let open = false
 
   let dispatch = createEventDispatcher()
 
-  function clickHandler(e) {
-    if (!e.target.classList.contains('clickHandler')) return
-    show = !show
-    if (show) dispatch('open')
+  function handleOpen() {
+    open = true
+    dispatch('open')
+  }
+
+  function handleToggle() {
+    open = !open
+    if (open) dispatch('open')
     else dispatch('close')
   }
 </script>
 
-<div class="simple-card">
-  <div on:click={clickHandler} class="clickable w3-row header clickHandler">
-    <div
-      class="w3-col s1 w3-right-align w3-large clickHandler"
-      style="padding-right: 20px;"
-    >
-      {count}
+<Card outlined hover={!open}>
+  <div
+    on:click={handleOpen}
+    class="pa-3 d-flex align-center {open ? '' : 'clickable'}"
+  >
+    <div on:click|stopPropagation>
+      <Button on:click={handleToggle} text fab size="small" class="mr-3">
+        <Icon path={mdiChevronRight} rotate={open ? 90 : 0} />
+      </Button>
     </div>
-    <div class="w3-col s9 w3-large clickHandler">
-      <i class="fas fa-chevron-right w3-opacity clickHandler" class:show />
-      &nbsp;{title}
-
+    <h6>
+      {count}
+      {title}
+    </h6>
+    <div class="flex-grow-1">
       <slot name="head" />
     </div>
-    <span class="w3-col s2 w3-right-align w3-large clickHandler">
+    <div>
       {renderAmount(sum)}
-    </span>
+    </div>
   </div>
 
-  {#if show}
+  {#if open}
     <div transition:slide|local>
-      {#if free}
-        <slot />
-      {:else if !items.length}
-        <div class="text-center text--secondary pt-2 pb-5">Aucun élément</div>
-      {:else if items.length}
-        <List style="padding: 7px;" {nonInteractive}>
-          {#each items as item}
-            <ListItem style="padding: 0 6px">
-              <span class="w3-col s1"><slot name="col-1" {item} /></span>
-              <span class="w3-col s8"><slot name="col-2" {item} /></span>
-              <span class="w3-col s3 w3-right-align"
-                ><slot name="col-3" {item} /></span
-              >
-            </ListItem>
-          {/each}
-        </List>
-      {/if}
+      <slot />
     </div>
   {/if}
-</div>
-
-<style>
-  .header {
-    padding: 15px 7px;
-    line-height: 32px;
-  }
-
-  .fa-chevron-right {
-    transition: 0.2s;
-  }
-
-  .fa-chevron-right.show {
-    transform: rotate(90deg);
-  }
-</style>
+</Card>
