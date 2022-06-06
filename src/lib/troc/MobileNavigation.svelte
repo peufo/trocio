e9e6b41cb921326988f79515f9a5435602d5fdcd
jonas <jonas.voisard@gmail.com>
2022-06-06 20:03:56 +0200

    Bind url with Mobile nav

--- src/lib/troc/MobileNavigation.svelte
+++ src/lib/troc/MobileNavigation.svelte
@@ -1,24 +1,32 @@
 <script lang="ts">
   import { mdiEarth, mdiPlus } from '@mdi/js'
-  import { goto, redirect } from '@roxi/routify'
+  import { page, redirect } from '@roxi/routify'
 
   import { Tabs, Tab, Icon } from 'svelte-materialify'
   import logo from '$assets/logo'
 
   const TABS = [
-    { icon: { path: mdiEarth }, label: 'Découvrir', href: './' },
-    { icon: logo, label: 'Mes trocs', href: './my' },
-    { icon: { path: mdiPlus }, label: 'Nouveau', href: './create' },
+    { icon: { path: mdiEarth }, label: 'Découvrir' },
+    { icon: logo, label: 'Mes trocs', href: 'my' },
+    { icon: { path: mdiPlus }, label: 'Nouveau', href: 'create' },
   ]
+  const foundTabIndex = TABS.findIndex((tab) => $page.title === tab.href)
+  let tabIndex = foundTabIndex > 0 ? foundTabIndex : 0
 
   function handleChange(event: { detail: number }) {
-    const tabIndex = event.detail
-    $goto(TABS[tabIndex].href, {})
+    tabIndex = event.detail
+    $redirect(`./${TABS[tabIndex].href || ''}`)
   }
 </script>
 
 <nav>
-  <Tabs icons grow class="secondary-color theme--dark" on:change={handleChange}>
+  <Tabs
+    icons
+    grow
+    class="secondary-color theme--dark"
+    on:change={handleChange}
+    value={tabIndex}
+  >
     <div slot="tabs">
       {#each TABS as tab}
         <Tab>
