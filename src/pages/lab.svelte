6f5bcd6398641c61cae1f7671c231877f1a39661
jonas <jonas.voisard@gmail.com>
2022-08-14 14:57:14 +0200

    lol with sensor

--- src/pages/lab.svelte
+++ src/pages/lab.svelte
@@ -1,39 +1,71 @@
-<script>
-  import { Button, Card } from 'svelte-materialify'
-  import { notify, success, info, warning, error } from '$lib/notify/index'
+<script lang="ts">
+  import { onMount } from 'svelte'
+  let x = 0
+  let y = 0
+  let z = 0
 
-  const text = 'prout'
-  const longText = new Array(10).fill(text).join(' ')
-  const extraLongText = new Array(100).fill(text).join(' ')
+  const frequency = 10
+  const reaction = 0.1
+
+  let dot = {
+    x: 0,
+    y: 0,
+    v: {
+      x: 0,
+      y: 0,
+    },
+  }
+
+  onMount(() => {
+    let acl = new Accelerometer({
+      frequency,
+    })
+
+    function reading() {
+      ;({ x, y, z } = acl)
+      dot.v.x -= acl.x / (frequency / reaction)
+      dot.v.y += acl.y / (frequency / reaction)
+
+      dot.x += dot.v.x
+      dot.y += dot.v.y
+    }
+
+    acl.addEventListener('reading', reading)
+    acl.start()
+    return () => {
+      acl.removeEventListener('reading', reading)
+    }
+  })
+
+  function handleReset() {
+    dot = {
+      x: 0,
+      y: 0,
+      v: {
+        x: 0,
+        y: 0,
+      },
+    }
+  }
 </script>
 
-<div class="pa-5">
-  <Card class="pa-5">
-    <Button on:click={() => notify(text, true)}>Prout persistent</Button>
-    <Button on:click={() => notify(extraLongText, true)}>
-      Extra long persistent
-    </Button>
-    <Button
-      on:click={() => {
-        notify({ message: longText, message: 'Long text', type: 'success' })
-      }}
-    >
-      Long text
-    </Button>
-    <Button
-      on:click={() => {
-        notify(extraLongText)
-      }}
-    >
-      Extra long text
-    </Button>
-    <br />
-    <Button on:click={() => success('YOLO')}>Success</Button>
-    <Button on:click={() => info('asd')}>Info</Button>
-    <Button on:click={() => warning('YOLO')}>warning</Button>
-    <Button on:click={() => error('YOLO')}>error</Button>
-  </Card>
+<div>
+  <button on:click={handleReset}>reset</button>
+  <span>x: {x.toFixed(3)}</span>
+  <span>y: {y.toFixed(3)}</span>
+  <span>z: {z.toFixed(3)}</span>
 </div>
 
+<div class="dot" style="transform: translate({dot.x}cm, {dot.y}cm);" />
+
 <style>
+  .dot {
+    position: absolute;
+    left: calc(50% - 10px);
+    top: calc(50% - 10px);
+    width: 20px;
+    height: 20px;
+    background: #9d9d9d;
+    border-radius: 50%;
+  }
 </style>
