d670849c403a06dea2661f2523e9718977ebd0b5
jonas <jonas.voisard@gmail.com>
2022-09-05 12:20:15 +0200

    Add params in redirect login

--- src/pages/admin/_layout.svelte
+++ src/pages/admin/_layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { params, goto } from '@roxi/routify'
+  import { params, goto, url } from '@roxi/routify'
 
   import Loader from '$lib/util/Loader.svelte'
   import NavigationAdmin from '$lib/layout/NavigationAdmin.svelte'
@@ -18,39 +18,41 @@
     onSuccess: troc.set,
   })
 
-  if (!$user) $goto('/trocs')
+  if (!$user) $goto('/login', { callback: $url(undefined, $params) })
 </script>
 
-{#if $trocQuery?.isLoading}
-  <div class="centered" style="height: 100px;">
-    <Loader />
-  </div>
-{:else if !$trocQuery || $trocQuery?.isError}
-  <div class="centered" style="height: 100px;">
-    <span>Oups, une erreur c'est produite.</span>
-  </div>
-{:else if $trocQuery.isSuccess && $troc}
-  <NavigationAdmin
-    bind:realWidth={navigationWidth}
-    on:openTips={() => (tipsActive = !tipsActive)}
-  />
+{#if !!$user}
+  {#if $trocQuery?.isLoading}
+    <div class="centered" style="height: 100px;">
+      <Loader />
+    </div>
+  {:else if !$trocQuery || $trocQuery?.isError}
+    <div class="centered" style="height: 100px;">
+      <span>Oups, une erreur c'est produite.</span>
+    </div>
+  {:else if $trocQuery.isSuccess && $troc}
+    <NavigationAdmin
+      bind:realWidth={navigationWidth}
+      on:openTips={() => (tipsActive = !tipsActive)}
+    />
 
-  <Tips bind:width={tipsWidth} bind:active={tipsActive} />
+    <Tips bind:width={tipsWidth} bind:active={tipsActive} />
 
-  <div
-    class="layout"
-    style="
-  padding-left: {navigationWidth};
-  padding-right: {tipsActive ? tipsWidth : '0px'};"
-  >
-    <main class="pa-4">
-      {#if $troc.is_try}
-        <div class="orange white-text alert mb-4">Troc d'entrainement</div>
-      {/if}
+    <div
+      class="layout"
+      style="
+        padding-left: {navigationWidth};
+        padding-right: {tipsActive ? tipsWidth : '0px'};"
+    >
+      <main class="pa-4">
+        {#if $troc.is_try}
+          <div class="orange white-text alert mb-4">Troc d'entrainement</div>
+        {/if}
 
-      <slot />
-    </main>
-  </div>
+        <slot />
+      </main>
+    </div>
+  {/if}
 {/if}
 
 <style>
