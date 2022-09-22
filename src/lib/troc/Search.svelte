43cf344b295dd5bc5c768a42081982eb2be4c1cd
jonas <jonas.voisard@gmail.com>
2022-09-22 15:15:09 +0200

    Change default filter

--- src/lib/troc/Search.svelte
+++ src/lib/troc/Search.svelte
@@ -10,8 +10,8 @@
   import { queryTrocsParams } from '$lib/troc/store'
   import IconLink from '$lib/util/IconLink.svelte'
 
-  // TODO: -2 year juste for dev
-  const initialStart = dayjs().add(-2, 'year').format('YYYY-MM-DD')
+  // const initialStart = dayjs().add(-2, 'year').format('YYYY-MM-DD')
+  const initialStart = dayjs().format('YYYY-MM-DD')
   const initialEnd = dayjs().add(1, 'month').format('YYYY-MM-DD')
   let searchElement: HTMLInputElement
   let startElement: HTMLInputElement
