318d82e55082d03972e2b555899422d4aa654dd3
jonas <jonas.voisard@gmail.com>
2022-09-22 15:51:50 +0200

    Remove beta advertissement

--- src/pages/trocs/create.svelte
+++ src/pages/trocs/create.svelte
@@ -1,8 +1,6 @@
 <script>
   import { onMount } from 'svelte'
   import { redirect, page } from '@roxi/routify'
-  import { Icon } from 'svelte-materialify'
-  import { mdiAlert } from '@mdi/js'
 
   import { user } from '$lib/user/store'
   import TrocEdit from '$lib/troc/Edit.svelte'
@@ -13,12 +11,6 @@
 
 {#if !!$user}
   <div class="pa-2">
-    <div class="orange white-text alert mb-4">
-      <Icon path={mdiAlert} class="mr-4" />
-
-      La création d'un troc est une fonctionnalité réservée aux testeurs.
-      Contactez-nous si vous souhaitez participer à la beta.
-    </div>
     <TrocEdit createMode />
   </div>
 {/if}
