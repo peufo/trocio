40b03ecc94837d793851158e2daf6c4dccccdbae
jonas <jonas.voisard@gmail.com>
2022-09-28 22:46:54 +0200

    Remove all advertissement in console 😎

--- src/material/components/Radio/Radio.svelte
+++ src/material/components/Radio/Radio.svelte
@@ -1,61 +1,64 @@
 <script context="module">
-  import uid from '../../internal/uid';
+  import uid from '../../internal/uid'
 </script>
 
 <script>
-  import TextColor from '../../internal/TextColor';
-  import Ripple from '../../actions/Ripple';
+  import TextColor from '../../internal/TextColor'
+  import Ripple from '../../actions/Ripple'
 
   // Add class to radio wrapper.
-  let klass = '';
-  export { klass as class };
+  let klass = ''
+  export { klass as class }
 
   // Color of the radio when active.
-  export let color = 'primary';
+  export let color = 'primary'
 
   // Disables the radio.
-  export let disabled = false;
+  export let disabled = false
 
   // Bind radio to a group.
-  export let group = [];
+  export let group = []
 
   // Value for the radio.
-  export let value = null;
+  export let value = null
 
   // Id for the checkbox.
-  export let id = null;
+  export let id = null
 
   // Styles for the radio wrapper.
-  export let style = null;
+  export let style = null
 
   // The <input/> element of the radio.
-  export let inputElement = null;
+  export let inputElement = null
 
-  id = id || `s-radio-${uid(5)}`;
-  $: active = group === value;
+  id = id || `s-radio-${uid(5)}`
+  $: active = group === value
 </script>
-
-<style lang="scss" src="./Radio.scss" global>
-</style>
 
 <div class="s-radio" {style}>
   <div
     class="s-radio__wrapper {klass}"
     class:disabled
     use:TextColor={!disabled && active && color}
-    use:Ripple={{ centered: true }}>
+    use:Ripple={{ centered: true }}
+  >
     <input
       type="radio"
       bind:this={inputElement}
       role="radio"
       aria-selected={active}
+      aria-checked={active}
       bind:group
       {id}
       {value}
-      {disabled} />
+      {disabled}
+    />
     <div class="s-radio__background" />
   </div>
   <label for={id}>
     <slot />
   </label>
 </div>
+
+<style lang="scss" src="./Radio.scss" global>
+</style>
