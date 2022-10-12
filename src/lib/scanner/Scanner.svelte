544baccf6c2ad21195cabf42e8ba58181635008f
jonas <jonas.voisard@gmail.com>
2022-10-13 00:00:40 +0200

    animation is perfect

--- src/lib/scanner/Scanner.svelte
+++ src/lib/scanner/Scanner.svelte
@@ -192,7 +192,10 @@
 
 <audio src={soundPristine} bind:this={audio} />
 
-<style>
+<style lang="scss">
+  $overlay-radius: 10px;
+  $diff-radius: 2.14601836px; // 10 * ((4 - PI) / 4)
+
   .scanner-wrapper {
     position: relative;
     display: grid;
@@ -224,45 +227,45 @@
     display: grid;
     place-items: center;
     outline: rgba(0, 0, 0, 0.2) 9999px solid;
-    border-radius: 10px;
+    border-radius: $overlay-radius;
     outline-offset: 4px;
-  }
 
-  .overlay > svg {
-    overflow: visible;
-  }
+    & > svg {
+      overflow: visible;
+    }
 
-  .overlay > svg > rect {
-    fill: transparent;
-    stroke-linecap: round;
-    animation-name: rotate-stroke;
-    animation-duration: 2000ms;
-    animation-iteration-count: infinite;
-    stroke: #fff;
-    stroke-width: 4;
-    stroke-opacity: 0.6;
-    stroke-dasharray: 49%;
-    stroke-dashoffset: 27%;
-  }
-  .overlay:not(.isProcessing):not(.isScanning) {
-    background: rgba(0, 0, 0, 0.6);
-    outline-offset: 0px;
-  }
+    & > svg > rect {
+      fill: transparent;
+      stroke-linecap: round;
+      animation-name: rotate-stroke;
+      animation-duration: 2000ms;
+      animation-iteration-count: infinite;
+      stroke: #fff;
+      stroke-width: 4px;
+      stroke-opacity: 0.6;
+      stroke-dasharray: calc(50% - $diff-radius);
+    }
 
-  .overlay.isProcessing > svg > rect {
-    animation-timing-function: linear;
-    animation-duration: 800ms;
-    stroke: #4456a8;
-    stroke-width: 8;
-    stroke-opacity: 1;
+    &:not(.isProcessing):not(.isScanning) {
+      background: rgba(0, 0, 0, 0.6);
+      outline-offset: 0px;
+    }
+
+    &.isProcessing > svg > rect {
+      animation-timing-function: linear;
+      animation-duration: 800ms;
+      stroke: #4456a8;
+      stroke-width: 8px;
+      stroke-opacity: 1;
+    }
   }
 
   @keyframes rotate-stroke {
     from {
-      stroke-dashoffset: 27%;
+      stroke-dashoffset: calc(25% + calc(4 * $diff-radius));
     }
     to {
-      stroke-dashoffset: 125%;
+      stroke-dashoffset: calc(125% + calc(2 * $diff-radius));
     }
   }
 </style>
