51aa88b6d2d4c24e8575fc5277004ffaa644fcf0
jonas <jonas.voisard@gmail.com>
2022-10-13 15:24:53 +0200

    rename _article -> article

--- src/lib/scanner/utils.ts
+++ src/lib/scanner/utils.ts
@@ -6,18 +6,17 @@ export async function getMismatchRaison(
   articleId: string,
   queryParams: { [key: string]: string }
 ): Promise<string> {
-  // Détermine la raison du mismatch
-  const [_article] = await api<Article[]>('/api/articles', {
+  const [article] = await api<Article[]>('/api/articles', {
     params: { exact__id: articleId },
   })
-  if (!_article) return 'Ne correspond pas à un article Trocio'
+  if (!article) return 'Ne correspond pas à un article Trocio'
 
   // Verifie le troc
   const exeptedTrocId = queryParams.exact_trocId
-  if (_article.trocId !== exeptedTrocId) return 'Ne correspond pas à ce troc'
+  if (article.trocId !== exeptedTrocId) return 'Ne correspond pas à ce troc'
 
   // Verifie l'état de l'article
-  const state = getState(_article)
+  const state = getState(article)
   const exeptedState = queryParams.exact_state as ArticleState
   if (state !== exeptedState) {
     const stateLabel = STATE_LABEL[state]
@@ -28,7 +27,7 @@ export async function getMismatchRaison(
   // Verifie les condition client
   const exeptedProviderSubId = queryParams.exact_providerSubId
   const notExeptedProviderSubId = queryParams.ne_exact_providerSubId
-  const providerSubId = _article.providerSubId
+  const providerSubId = article.providerSubId
   if (exeptedProviderSubId && exeptedProviderSubId !== providerSubId) {
     if (state === 'proposed') return `N'a pas été proposé par le client`
     if (state === 'valided') return `N'a pas été déposé par le client`
