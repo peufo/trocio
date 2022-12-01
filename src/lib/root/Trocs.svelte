b0f66c87c0f2c58f401c49092b1f3e164fb55cda
jonas <jonas.voisard@gmail.com>
2022-12-01 17:55:28 +0100

    Admin table for trocs

---
+++ src/lib/root/Trocs.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  import { mdiTrashCanOutline } from '@mdi/js'
+
+  import type { DynamicQuery, Troc, TrocLookup } from 'types'
+  import { List, ListItem, Icon } from '$material'
+  import { useInfinitApi } from '$lib/api'
+  import { layout } from '$lib/store/layout'
+  import { fieldsTroc } from '$lib/root/fieldsTroc'
+  import notify from '$lib/notify'
+  import MagicTable from '$lib/util/MagicTable.svelte'
+
+  let searchValue = ''
+  let queryParams = {}
+  $: query = useInfinitApi<DynamicQuery<Troc>, TrocLookup>([
+    'root/trocs',
+    {
+      or_search_name: searchValue,
+      or_search__id: searchValue,
+      ...queryParams,
+    },
+  ])
+
+  async function deleteTroc(troc: Troc) {
+    if (prompt(`Tapez "${troc.name}" pour le supprimer`) === troc.name) {
+      try {
+        let res = await fetch('/api/root/remove-troc', {
+          method: 'post',
+          body: JSON.stringify({ trocId: troc._id }),
+          headers: {
+            'Content-type': 'application/json',
+          },
+        })
+        let json = await res.json()
+        if (json.error) throw json.message
+        notify.success(json.message)
+      } catch (error: any) {
+        notify.error(error)
+      }
+    } else {
+      notify.warning('Nom incorrect')
+    }
+  }
+</script>
+
+<div class="pa-4">
+  <MagicTable
+    searchColSpan={1}
+    {query}
+    fields={fieldsTroc}
+    bind:queryParams
+    bind:searchValue
+    wrapperStyle="max-height: {$layout.mainHeight - 76}px;"
+  >
+    <h6 slot="title">Trocs</h6>
+
+    <div slot="menu" let:item let:menu>
+      <List dense>
+        <ListItem
+          class="red-text"
+          on:click={() => {
+            menu.close()
+            deleteTroc(item)
+          }}
+        >
+          <div slot="prepend">
+            <Icon path={mdiTrashCanOutline} />
+          </div>
+          Supprimer
+        </ListItem>
+      </List>
+    </div>
+  </MagicTable>
+</div>
---
+++ src/lib/root/fieldsTroc.ts
@@ -0,0 +1,13 @@
+import type { TrocLookup } from 'types'
+import type { FieldInteface } from 'types/magic'
+
+export const fieldsTroc: FieldInteface<TrocLookup>[] = [
+  {
+    label: 'Nom',
+    key: 'name',
+  },
+  {
+    label: 'creator',
+    key: 'creator',
+  },
+]
