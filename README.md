5666ab89e048e6eb85fa242a7dbc3a1c494cda05
jonas <jonas.voisard@gmail.com>
2022-12-01 22:39:18 +0100

    do a screenshot seption

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Plateforme pour l'organisation et gestion de commerces éphémères.
 
 👉 [Accéder à Troc.io](https://troc.io)
 
-![Screenshots de trocio](./src/assets/images/screenshots.svg)
+![Screenshots de trocio](./src/assets/images/screenshots.png)
--- src/pages/lab.svelte
+++ src/pages/lab.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import screenshots from '$assets/images/screenshots.svg?raw'
+  import screenshots from '$assets/images/screenshots.svg?url'
   console.log({ screenshots })
 </script>
 
-{@html screenshots}
+<img src={screenshots} alt="screenshot svg" />
