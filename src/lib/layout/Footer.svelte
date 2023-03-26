c96b7d3b89a7c0874ef78114bb31f4b9eaaf495f
jonas <jonas.voisard@gmail.com>
2023-03-26 14:12:04 +0200

    Fix mobile layout

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -10,45 +10,43 @@
   class="text-center grey {$isDarkTheme ? 'darken-3' : 'lighten-3'}"
   bind:offsetHeight
 >
-  <div>
-    <div class="links">
-      <div class="underline-div">
-        <a class="underline-span" href="/contact">Contact</a>
-      </div>
-      <div class="underline-div">
-        <a class="underline-span" href="/terms-of-use">
-          Conditions d'utilisation
-        </a>
-      </div>
-      <div class="underline-div">
-        <a class="underline-span" href="/legal-notice"> Mentions légales </a>
-      </div>
-
-      <Button href="https://github.com/PeufOne/trocio">
-        <Icon path={mdiGithub} class="mr-2" />
-        code source
-      </Button>
+  <div class="links">
+    <div class="underline-div">
+      <a class="underline-span" href="/contact">Contact</a>
+    </div>
+    <div class="underline-div">
+      <a class="underline-span" href="/terms-of-use">
+        Conditions d'utilisation
+      </a>
+    </div>
+    <div class="underline-div">
+      <a class="underline-span" href="/legal-notice"> Mentions légales </a>
     </div>
 
-    <span>
-      <i class="far fa-copyright" />
-      2018 - 2022 Jonas Voisard
-    </span>
+    <Button href="https://github.com/PeufOne/trocio">
+      <Icon path={mdiGithub} class="mr-2" />
+      code source
+    </Button>
   </div>
+
+  <div>Site réalisé par <a href="https://github.com/peufo">peufo</a></div>
 </footer>
 
 <style>
   footer {
+    padding: 2em;
     background: #efefef;
-    height: 100px;
     display: grid;
     place-items: center;
+    gap: 1em;
   }
 
   .links {
     display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
     align-items: center;
-    gap: 2em;
+    gap: 1em 2em;
     padding-bottom: 0.5em;
   }
 </style>
